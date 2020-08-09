1a32b53e154f59e3a7d431474bb6d178262775ed
Vipul Khandelwal <khandelwal.vipul1605@gmail.com>
2020-08-09 18:39:44 +0530

    changes

--- 06.Challenges-Fundamentals/02.Pythagorean_Triplet(Approach_I).cpp
+++ 06.Challenges-Fundamentals/02.Pythagorean_Triplet(Approach_I).cpp
@@ -16,7 +16,7 @@ Sample Output
 4 5
 
 Logic:-
-If m>n>0 are integers, then (m2-n2, 2mn, m2-n2) is a Pythagorean triple. This is easily seen with a bit of algebra. Thus, plugging in various (m,n) will give various triples.
+If m>n>0 are integers, then (m2-n2, 2mn, m2+n2) is a Pythagorean triple. This is easily seen with a bit of algebra. Thus, plugging in various (m,n) will give various triples.
 Now, our goal is to find a triple of the form (m2-n2, 2mn, m2-n2) in which our input value is one of the catheti. Note that the catheti are m2-n2 and 2mn. Also, note that the second cathetus is always even. Thus, it makes sense to consider two cases:
 
 If a is even, then let's try to equate it with the second cathetus, 2mn, i.e., let's try to find an (m,n) pair such that 2mn = a. In this case, is even, so we can divide by 2. We have mn = a/2. Thus, all we have to do is to find a factorization of a/2 into mn such that m>n. You can try various factoring methods — even naïve O(√a) time method will pass - but in this case , we don't really even need to do that, since the trivial (m,n) = (a/2,1) works!. This gives us the solution triple ( (a^2)/4 - 1, a, (a^2)/4 + 1 ). Note that (a^2)/4 is an integer since a is even.
