94e7591651b468906dcaf97a932c1520fffedc1f
Vipul Khandelwal <khandelwal.vipul1605@gmail.com>
2020-08-10 04:13:08 +0530

    changes to bubble sort

--- 09.Arrays/06.Bubble_Sort.cpp
+++ 09.Arrays/06.Bubble_Sort.cpp
@@ -6,10 +6,10 @@ using namespace std;
 void bubble_sort(int a[], int n) {
 
 	//N-1 large elements to the end
-	for (int itr = 1; itr <= n - 1; itr++) {
+	for (int itr = 0; itr < n - 1; itr++) {
 
 		//Pairwise Swapping in the unsorted of the array
-		for (int j = 0; j <= (n - itr - 1); j++) {
+		for (int j = 0; j < (n - itr - 1); j++) {
 			if (a[j] > a[j + 1]) {
 				swap(a[j], a[j + 1]);
 			}
