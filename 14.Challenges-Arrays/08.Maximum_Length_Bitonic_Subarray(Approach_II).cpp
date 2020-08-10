b15e6dea4eca396d9b7bfaa7628f3d8ab57f95c8
Vipul Khandelwal <khandelwal.vipul1605@gmail.com>
2020-08-10 20:55:56 +0530

    few change

--- 14.Challenges-Arrays/08.Maximum_Length_Bitonic_Subarray(Approach_II).cpp
+++ 14.Challenges-Arrays/08.Maximum_Length_Bitonic_Subarray(Approach_II).cpp
@@ -21,50 +21,44 @@ Sample Output
 4
 Explanation
 ForMaximum length = 4, 78, 90, 45, 23
-
-Logic:-
-We create two arrays - 'inc' and 'dec'
-
-inc[i] stores the length of increasing subarray till i.
-dec[i] stores the length of decreasing subarray starting from index i.
-Doing so gives us the length of increasing and decreasing subarray at each index in O(n) time.
-We calculate the length of the longest bitonic subarray by finding the maximum inc[i] + dec[i] - 1
-We subtract one since the current element at ith index is included in both the increasing and decreasing subarray lengths.
-Algorithm
-Initialize inc[0] to 1 and dec[n-1] to 1
-
-Creating inc[] array
-a. Till end of the array ie, i=1 to n, if arr[i] > arr[i-1] then inc[i] = inc[i-1] + 1. else, inc[i] = 1
-
-Creating dec[] array
-a. From the end of the array ie, i = n-2 till i =0, if arr[i] > arr[i+1] then dec[i] = dec[i+1] +1 else, dec[i] = 1
 */
 
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
 int bitonic(int arr[], int n)
 {
-    int inc[n]; // Length of increasing subarray ending at all indexes
-    int dec[n]; // Length of decreasing subarray starting at all indexes
-    int i, max;
-
-    // length of increasing sequence ending at first index is 1
-    inc[0] = 1;
-
-    // length of increasing sequence starting at first index is 1
-    dec[n-1] = 1;
-
-    // Step 1) Construct increasing sequence array
-    for (i = 1; i < n; i++)
-    inc[i] = (arr[i] >= arr[i-1])? inc[i-1] + 1: 1;
-
-    // Step 2) Construct decreasing sequence array
-    for (i = n-2; i >= 0; i--)
-    dec[i] = (arr[i] >= arr[i+1])? dec[i+1] + 1: 1;
-
-    // Step 3) Find the length of maximum length bitonic sequence
-    max = inc[0] + dec[0] - 1;
-    for (i = 1; i < n; i++)
-        if (inc[i] + dec[i] - 1 > max)
-            max = inc[i] + dec[i] - 1;
-
-    return max;
+	int inc[n]; // Length of increasing subarray ending at all indexes
+	int dec[n]; // Length of decreasing subarray starting at all indexes
+	int i, max;
+	// length of increasing sequence ending at first index is 1
+	inc[0] = 1;
+	// length of increasing sequence starting at first index is 1
+	dec[n-1] = 1;
+	// Step 1) Construct increasing sequence array
+	for (i = 1; i < n; i++)
+	inc[i] = (arr[i] >= arr[i-1])? inc[i-1] + 1: 1;
+	// Step 2) Construct decreasing sequence array
+	for (i = n-2; i >= 0; i--)
+	dec[i] = (arr[i] >= arr[i+1])? dec[i+1] + 1: 1;
+	// Step 3) Find the length of maximum length bitonic sequence
+	max = inc[0] + dec[0] - 1;
+	for (i = 1; i < n; i++)
+	if (inc[i] + dec[i] - 1 > max)
+	max = inc[i] + dec[i] - 1;
+	return max;
+}
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+		int n;
+		cin >> n;
+		int arr[n];
+		for(int i=0; i<n; i++){
+			cin >> arr[i];
+		}
+		cout << bitonic(arr, n) << endl;
+	}
+	return 0;
 }
