73ce4e35b509b08dc4df37e0669c7cc87e267dd5
Vipul Khandelwal <khandelwal.vipul1605@gmail.com>
2020-08-25 13:51:08 +0530

    changes

--- 04.Programming_Fundamentals-1/10.Square_Root.cpp
+++ 04.Programming_Fundamentals-1/10.Square_Root.cpp
@@ -27,5 +27,45 @@ int main() {
 
 	cout << ans << endl;
 
+    /*
+    Inbuilt Functions to compute square root:-
+    1. (For double)sqrt:- It returns the square root of a number to type double
+    2. (For long long)sqrtl:- It returns the square root of a number to type long double with more precision.
+    3. (For float)sqrtf
+    Left mentioned things are what value they return and in all of the above sqrtl has most precision
+
+    Usage:-
+    1.
+    double val1 = 300.0;
+
+    cout << fixed << setprecision(12) << sqrt(val1) << endl;
+
+    Output:
+    17.320508075689
+
+    2.
+    long long int val1 = 1000000000000000000;
+    long long int val2 = 999999999999999999;
+
+    cout << fixed << setprecision(12) << sqrt(val1) << endl;
+    cout << fixed << setprecision(12) << sqrt(val2) << endl;
+
+    Output:
+    1000000000.000000000000
+    999999999.999999999476
+
+    3.
+    float val1 = 225.0;
+    float val2 = 300.0;
+
+    cout << fixed << setprecision(12) << sqrtf(val1) << endl;
+    cout << fixed << setprecision(12) << sqrtf(val2) << endl;
+
+    Output:
+    15.000000000000
+    17.320508956909
+
+    */
+
 	return 0;
 }
