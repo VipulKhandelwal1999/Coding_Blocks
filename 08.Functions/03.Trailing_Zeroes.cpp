72efc80f2063900b8cd7fd4a6d98a2529255c866
Vipul Khandelwal <khandelwal.vipul1605@gmail.com>
2020-08-26 12:45:19 +0530

    changes

--- 08.Functions/03.Trailing_Zeroes.cpp
+++ 08.Functions/03.Trailing_Zeroes.cpp
@@ -1,3 +1,25 @@
+/*
+Given an integer n, write a function that returns count of trailing zeroes in n!.
+
+
+
+Input Format
+A single integer N.
+
+Constraints
+1 <= N <= 10^9
+
+Output Format
+A single integer denoting the count of trailing zeroes in N!
+
+Sample Input
+5
+Sample Output
+1
+Explanation
+Factorial of 5 is 120 which has one trailing 0.
+*/
+
 #include<iostream>
 using namespace std;
 
