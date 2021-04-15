f8d119e77095a476a0b76803e6b0cb3b06b0bce1
Vipul Khandelwal <khandelwal.vipul1605@gmail.com>
2021-04-15 20:10:23 +0530

    Added Codes

--- 11.Character-Arrays/04.Remove_Consecutive_Duplicates.cpp
+++ 11.Character-Arrays/04.Remove_Consecutive_Duplicates.cpp
@@ -1,24 +1,23 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-//remove consecutive duplicate characters from a string
-//ccoooding -- > coding
+// remove consecutive duplicate characters from a string
+// ccoooding --> coding
 
 
-void removeDuplicates(char a[]) {
-
+void removeDuplicates(char a[]){
 	int l = strlen(a);
 	if (l == 1 or l == 0) {
 		return;
 	}
-
 	int prev = 0;
-	for (int current = 1; current < l; current++) {
-		if (a[current] != a[prev]) {
+	for (int current = 1; current < l; current++){
+		if(a[current] != a[prev]){
 			prev++;
 			a[prev] = a[current];
 		}
 	}
 	a[prev + 1] = '\0';
+
 	return;
 }
 
@@ -28,9 +27,6 @@ int main() {
 	cin.getline(a, 1000);
 	removeDuplicates(a);
 	cout << a << endl;
-
-
-
 
 	return 0;
 }
